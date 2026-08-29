9acbce5ae112ed746bc7fc0924a05cf7d76fde0d
Rashmi <rashmigowda1308@gmail.com>
2026-08-29 17:44:42 +0530

    Make the cart quantiy interactive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -84,6 +84,16 @@ products.forEach((product)=>{
             })
             }
 
+            let cartQuantity = 0;
+
+            cart.forEach((item) => {
+                cartQuantity += item.quantity
+            })
+
+            document.querySelector('.js-cart-quantity')
+            .innerHTML = cartQuantity
+
+
         });
     });
 });
