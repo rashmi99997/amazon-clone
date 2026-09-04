697c56f2a608e248b98aefabc1e589ff12d0d911
Rashmi <rashmigowda1308@gmail.com>
2026-09-04 13:22:56 +0530

    Create automated test for fomatCurrency

---
+++ javascript-amazon-project-main/tests/moneyTest.js
@@ -0,0 +1,24 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+console.log('test suite: formatCurrency');
+
+console.log('convert cents to dollars and round to 2 decimal places');
+if(formatCurrency(2095) === '20.95') {
+  console.log('Test passed');
+} else {
+  console.log('Test failed');
+}
+
+console.log('works with 0 cents');
+if(formatCurrency(0) === '0.00') {
+  console.log('Test passed');
+} else {
+  console.log('Test failed');
+}
+
+console.log('rounds upto the nearest cent');
+if(formatCurrency(2000.5) === '20.01') {
+  console.log('Test passed');
+} else {
+  console.log('Test failed');
+}
