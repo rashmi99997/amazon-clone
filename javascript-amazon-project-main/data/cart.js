bb0878881fcb2999d2a0a87386412b5f9bb690b8
Rashmi <rashmigowda1308@gmail.com>
2026-09-04 15:56:54 +0530

    Create automated tests for addToCart

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,4 +1,10 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage()
+{
+    cart = JSON.parse(localStorage.getItem('cart'));
 
 if(!cart)
 {
@@ -16,7 +22,7 @@ if(!cart)
     }
 ];
 }
-
+}
 function saveToStorage()
 {
     localStorage.setItem('cart',JSON.stringify(cart));
--- javascript-amazon-project-main/test-jasmine/utils/moneyTest.js
+++ javascript-amazon-project-main/test-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import {formatCurrency} from '../scripts/utils/money.js';
+import {formatCurrency} from '../../scripts/utils/money.js';
 
 describe('test suite: formatCurrency', () => {
     it('convert cents to dollars and round to 2 decimal places', () => {
