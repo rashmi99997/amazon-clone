85c44379993f64efd6f6e57badfcb23ce9356266
Rashmi <rashmigowda1308@gmail.com>
2026-09-05 19:42:04 +0530

    Load products from the backend

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -100,10 +100,33 @@ class Clothing extends Product
   objects3.method();
   */
   
+ export let products = [];
+
+export function loadProducts(fun)
+ {
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load',() =>
+  {
+    products = JSON.parse(xhr.response).map((productDetails) =>
+  {
+    if (productDetails.type === "clothing")
+    {
+      return new Clothing(productDetails);
+    }
+    return new Product(productDetails);
+  }
+  );
+
+    console.log('load products');
+    fun();
+  })
+  xhr.open('GET','https://supersimplebackend.dev/products');
+  xhr.send();
+ }
+ 
 
 
-
-
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -772,4 +795,4 @@ export const products = [
     return new Product(productDetails);
   }
 );
-
+*/
--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,8 +1,10 @@
 import {cart, addToCart} from '../data/cart.js';
-import { products } from '../data/products.js';
+import { products,loadProducts } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
-
+loadProducts(renderProductGrid);
+function renderProductGrid()
+{
 let productsHTML = '';
 
 products.forEach((product)=>{
@@ -92,3 +94,4 @@ products.forEach((product)=>{
         });
     });
 });
+}
