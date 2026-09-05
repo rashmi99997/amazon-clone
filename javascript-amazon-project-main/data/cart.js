8845a36269c801a9607d97b6306ce8f7db79b3b6
Rashmi <rashmigowda1308@gmail.com>
2026-09-05 20:36:59 +0530

    Learn promises and convert our code to promises

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -87,3 +87,15 @@ export function addToCart(productId)
             matchingItem.deliveryOptionId = deliveryOptionId;
             saveToStorage();
     }
+
+    export function loadCart(fun)
+     {
+      const xhr = new XMLHttpRequest();
+      xhr.addEventListener('load',() =>
+      {
+       console.log(xhr.response)
+        fun();
+      })
+      xhr.open('GET','https://supersimplebackend.dev/cart');
+      xhr.send();
+     }
--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,11 +1,57 @@
-import { renderOrderSummary } from "./checkout/orderSummary.js";
-import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-//import '../data/cart-class.js'; 
-//import '../data/backend-practice.js'  
-import { loadProducts } from "../data/products.js";
+import { renderOrderSummary } from "./checkout/orderSummary.js"; 
+import { renderPaymentSummary } from "./checkout/paymentSummary.js"; 
+//import '../data/cart-class.js';  
+//import '../data/backend-practice.js'   
+import { loadProducts } from "../data/products.js"; 
+import {loadCart} from '../data/cart.js'; 
+ 
+Promise.all([ 
+    new Promise((resolve) => 
+    { 
+        loadProducts(() => 
+        { 
+            resolve(); 
+        })
+    }), // comma was missing here
 
-loadProducts(() =>
-{
+    new Promise((resolve) => 
+    { 
+        loadCart(() => 
+        { 
+            resolve(); 
+        }); 
+    }) 
+ 
+]).then((values) => {
+    console.log(values)
     renderOrderSummary();
     renderPaymentSummary();
-});
+}); 
+
+
+/* 
+new Promise((resolve) => 
+{ 
+    loadProducts(() => 
+    { 
+        resolve(); 
+    }) 
+}).then(() => 
+{ 
+    renderOrderSummary(); 
+    renderPaymentSummary(); 
+}); 
+*/ 
+     
+ 
+/* 
+loadProducts(() => 
+{ 
+    loadCart(() => 
+    { 
+        renderOrderSummary(); 
+        renderPaymentSummary(); 
+    }) 
+     
+}); 
+*/
